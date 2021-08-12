b883a5fe6a0b94c2d99a79c124f8485aa45945e8
Tom Hine <tomhine792@gmail.com>
2021-08-12 09:28:42 +0100

    fix product images, give product list max width

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,22 +1,18 @@
-import { useContext } from "react";
-import styles from "./ProductList.module.css";
-import products from "../../dev-data/products.json";
-import img from "../../images/tcon-prod-img.jpg";
-import { cartCtx } from "../../context/CartContext";
+import { useContext } from 'react';
+import styles from './ProductList.module.css';
+import products from '../../dev-data/products.json';
+import { cartCtx } from '../../context/CartContext';
 
-const ProductList = (props) => {
+const ProductList = () => {
   const cartContext = useContext(cartCtx);
 
-  const productCards = products.map((product) => (
+  const productCards = products.map(product => (
     <div className={styles.productCard} key={product.id}>
-      <img src={img} alt="concrete screws in box" />
+      <img src={`/images${product.image_url}`} alt="concrete screws in box" />
       <div className={styles.productInfo}>
         <h4>{product.name}</h4>
         <p>{product.sku}</p>
-        <button
-          className={styles.addToCart}
-          onClick={() => cartContext.addProductToCart(product)}
-        >
+        <button className={styles.addToCart} onClick={() => cartContext.addProductToCart(product)}>
           Add to Cart
         </button>
       </div>
